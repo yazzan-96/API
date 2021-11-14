cfac0393cb066eec7a4fb51ae381fdd7e20fc085
yazzan-96 <yazanaraddadi@gmail.com>
2021-11-14 16:36:07 +0300

    projects done

--- API/ViewController.swift
+++ API/ViewController.swift
@@ -6,14 +6,44 @@
 //
 
 import UIKit
-
+struct Facts : Codable {
+    var fact: String
+}
 class ViewController: UIViewController {
+    
+   
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        getCharacters ()
+    }
+    func getCharacters () {
+        var urlComponents = URLComponents()
+        urlComponents.scheme = "https"
+        urlComponents.host = "dog-facts-api.herokuapp.com"
+        urlComponents.path = "/api/v1/resources/dogs/all"
+        var urlRequest = URLRequest (url:urlComponents.url!)
+        let urlSession = URLSession.shared
+        let task = urlSession.dataTask(with: urlRequest) { (data: Data?, response: URLResponse?, error: Error?) in
+            do {
+                let JSONDecoder = JSONDecoder()
+                let Characters = try JSONDecoder.decode([Facts].self, from: data!)
+                print (Characters)
+            }
+            catch {
+                print("err:\(error)")
+            }
+            }
+        task.resume()
+        
+            
+        }
+        
+        
         // Do any additional setup after loading the view.
     }
 
 
-}
+
 
